b447be6a359cefa542c8ebe535b7213899f392f9
fruitteeeeea <138597264+fruitteeeeea@users.noreply.github.com>
2024-03-23 00:32:23 +0800

    0323edit_at_home

--- script/bottom_button.gd
+++ script/bottom_button.gd
@@ -4,8 +4,6 @@ extends Button
 @export var position_show = Vector2(144.5, 630)
 @export var position_hide = Vector2(144.5, 900)
 
-@onready var dock = $dock
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
